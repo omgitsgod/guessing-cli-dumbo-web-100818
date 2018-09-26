fddca30238d7590445079f516ba9567af8c7b130
github username <github email address>
2018-09-26 18:03:58 -0400

    Done.

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -4,14 +4,6 @@ puts "Guess a number between 1 and 6."
 guess = gets.chomp
 numr = rand(1..6)
 
-#if guess == "exit"
-#  puts "Goodbye!"
-# elsif guess.to_i == numr
-#  puts "You guessed the correct number!"
-# else
-#  puts "The computer guessed #{numr}."
-# ßend
-
 while guess != "exit"
   if guess.to_i == numr
     puts "You guessed the correct number!"
